3bb85c69c5212ae3d11ce254b07c137a7a70a02f
bri-guy-tries <bri.guy.tries@gmail.com>
2024-06-07 04:01:43 -0400

    Delete briguy_portfolio directory

--- briguy_portfolio/src/app/projects-list/page.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata: Metadata = {
-    title: "Project List",
-    description: "this is the project list page",
-};
-export default function projectList()
-
-{
-    return <h1>THis is our project list page</h1>
-}
